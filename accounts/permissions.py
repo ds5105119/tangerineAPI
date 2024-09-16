de65b62e1336f78434a5c1bfc7cad0795a88d6f7
IIH <ds5105119@gmail.com>
2024-09-17 01:39:36 +0900

    CRUD 기능 전부를 구현합니다. profile 또한 수정할 수 있습니다. 단, 유저 자신에게 다른 유저 추천 기능은 차후 구현 예정입니다. (ML 사용 기능은 차후 개발입니다.) 자세한 사항은 /api/schema/swagger-ui/#/accounts를 참조하세요.

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -4,14 +4,23 @@ except ImportError:
     raise ImportError("django-rest-framework needs to be added to INSTALLED_APPS.")
 
 
-class UserDataUpdatePermission(BasePermission):
+class UserPermission(BasePermission):
+    """
+    retrieve: 유저 프로필 개별 조회는 모든 사람이 가능
+    list: 유저 프로필 여러개 조회는 로그인 한 사람만 가능
+    partial_update, update, destroy: 유저 프로필 부분 업데이트, 업데이트 및 삭제는 해당 유저 또는 관리자만 가능
+    """
+
     def has_permission(self, request, view):
-        if request.method in ["PUT", "PATCH"]:
+        if view.action == "retrieve":
             return True
+        if view.action in ["list", "partial_update", "update", "destroy"]:
+            return request.user and request.user.is_authenticated
+        return False
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_superuser or request.user.is_staff:
+        if view.action == "retrieve":
             return True
-        elif obj.user == request.user:
-            return True
+        if view.action in ["partial_update", "update", "destroy"]:
+            return request.user.is_staff or obj == request.user
         return False
