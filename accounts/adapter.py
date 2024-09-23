096e56ea9979863035db86a386a6110c9336d410
IIH <ds5105119@gmail.com>
2024-09-23 23:02:53 +0900

    일반 유저 가입 시에 오류를 수정하였습니다. allauth의 DefaultAccountAdapter를 오버라이드 하였습니다. 기본 설정에서 이메일 인증을 비활성화 하고, 차후 이메일 인증을 위한 이메일 백엔드 설정을 하였습니다.

--- accounts/adapter.py
+++ accounts/adapter.py
@@ -3,7 +3,7 @@ from profiles.models import *
 
 try:
     from allauth.account.adapter import DefaultAccountAdapter
-    from allauth.account.utils import user_email
+    from allauth.account.utils import user_email, user_username
     from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
     from allauth.utils import valid_email_or_none
     from django.contrib.auth import get_user_model
@@ -16,11 +16,41 @@ User = get_user_model()
 
 class AccountAdapter(DefaultAccountAdapter):
     """
-    Custom Account Adapter(Dummy)
-    Adapter for custom accounts, but IIHUS will not provide normal login.
+    Custom Account Adapter
     """
 
-    pass
+    def save_user(self, request, user, form, commit=True):
+        """
+        Saves a new `User` instance using information provided in the
+        signup form.
+        """
+
+        data = form.cleaned_data
+
+        email = data.get("email")
+        username = data.get("username")
+        handle = generate_handle()
+        while User.objects.filter(handle=handle).exists():
+            handle = generate_handle()
+        profile = Profile.objects.create()
+
+        user.email = email
+        user_username(user, username)
+        if "password1" in data:
+            user.set_password(data["password1"])
+        elif "password" in data:
+            user.set_password(data["password"])
+        else:
+            user.set_unusable_password()
+        self.populate_username(request, user)
+        user.handle = handle
+        user.profile = profile
+
+        if commit:
+            # Ability not to commit makes it easier to derive from
+            # this adapter by adding
+            user.save()
+        return user
 
 
 class SocialAccountAdapter(DefaultSocialAccountAdapter):
@@ -48,7 +78,7 @@ class SocialAccountAdapter(DefaultSocialAccountAdapter):
         profile = Profile.objects.create()
 
         user = sociallogin.user
-        user.name = name or ""
+        user.username = name or ""
         user_email(user, valid_email_or_none(email) or "")
         user.handle = handle
         user.profile = profile
