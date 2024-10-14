b76d28fc7ad4331b9de373eeee32eb03ddc8eb05
IIH <ds5105119@gmail.com>
2024-10-14 14:18:30 +0900

    url 추가

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 
 from .views import (
+    LatestPostsViaFollowAPIView,
     LatestPostsViaHandleAPIView,
     PostViewSet,
 )
@@ -12,8 +13,13 @@ router.register(r"p", PostViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     re_path(
-        r"latest/(?P<handle>[\w.]+)/",
+        r"latest/(?P<handle>[a-z0-9._]+)/",
         LatestPostsViaHandleAPIView.as_view(),
+        name="user-latest-posts",
+    ),
+    re_path(
+        r"follows/(?P<handle>[a-z0-9._]+)/",
+        LatestPostsViaFollowAPIView.as_view(),
         name="user-latest-posts",
     ),
 ]
